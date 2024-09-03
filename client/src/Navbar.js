e767b7d80d48d28c282835f88672e91782a6c8e5
Ochieng John Ronnie <johnronnie18@gmail.com>
2024-09-04 01:16:03 +0300

    ababout us page made

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,24 +1,25 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'; // Import Nav component
 
 export default function NavBar() {
   return (
     <Navbar bg="primary" variant="dark" expand="lg" style={{ padding: '10px 20px' }}>
       <Container>
         {/* Logo */}
-        <Navbar.Brand href="#home" style={{ color: 'white', fontSize: '36px', fontWeight: 'bold' }}>
-          RBS
+        <Navbar.Brand as={Link} to="/" style={{ color: 'white', fontSize: '36px', fontWeight: 'bold' }}>
+          Nasyjma Autos
         </Navbar.Brand>
         {/* Collapse button for mobile */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         {/* Navbar items */}
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           <Nav>
-            <Nav.Link href="#home" style={{ color: 'white', fontSize: '18px' }}>Home</Nav.Link>
-            <Nav.Link href="#aboutus" style={{ color: 'white', fontSize: '18px' }}>About Us</Nav.Link>
-            <Nav.Link href="#contactus" style={{ color: 'white', fontSize: '18px' }}>Contact Us</Nav.Link>
-            <Nav.Link href="#services" style={{ color: 'white', fontSize: '18px' }}>Services</Nav.Link>
+            <Nav.Link as={Link} to="/home" style={{ color: 'white', fontSize: '18px' }}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/aboutus" style={{ color: 'white', fontSize: '18px' }}>About Us</Nav.Link>
+            <Nav.Link as={Link} to="/contactus" style={{ color: 'white', fontSize: '18px' }}>Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/services" style={{ color: 'white', fontSize: '18px' }}>Services</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
